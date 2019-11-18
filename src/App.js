c3fe68837e5ccb3b16cee31261f54f5039d3191b
adrian hernandez <adrianhh1426@gmail.com>
2019-11-18 14:57:21 -0600

    función de eliminar citas

--- src/App.js
+++ src/App.js
@@ -19,6 +19,20 @@ class App extends Component {
     });
   }
 
+  //eliminar citas del state
+  eliminarCita = id =>{
+    //tomar una copia del state, no mutarlo directamente, siempre hacer copia
+    const citasActuales = [...this.state.citas];
+
+    //filter para excluir el id pasado
+    const citas = citasActuales.filter(cita => cita.id !== id);
+
+    //actualizar el state
+    this.setState({
+      citas
+    });
+  }
+
   render() {
     return (
       <div className="container">
@@ -38,6 +52,7 @@ class App extends Component {
         <div className="mt-5 col-md-10 mx-auto">
           <ListaCitas
             citas={this.state.citas}
+            eliminarCita={this.eliminarCita}
           />
         </div>
         
