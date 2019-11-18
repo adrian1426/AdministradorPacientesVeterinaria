3fcd5325ac9df5963b52073d55fa9d2314803a81
adrian hernandez <adrianhh1426@gmail.com>
2019-11-18 16:19:17 -0600

    mensaje de lista de citas, personalizado

--- src/components/ListaCitas.js
+++ src/components/ListaCitas.js
@@ -2,10 +2,15 @@ import React from 'react';
 import Cita from './Cita';
 import Proptypes from 'prop-types';
 
-const ListaCitas = ({citas,eliminarCita}) =>(
-    <div className="card mt-2 py-5">
+const ListaCitas = ({citas,eliminarCita}) =>{
+
+    //obtener citas y el mensaje a mostrar
+    const mensaje = Object.keys(citas).length === 0 ? 'No hay citas' : 'Administrar citas'
+
+    return(
+        <div className="card mt-2 py-5">
             <div className="card-title text-center">
-            Administra las citas aquí!
+            {mensaje}
             </div>
 
             <div className="lista-citas">
@@ -19,8 +24,9 @@ const ListaCitas = ({citas,eliminarCita}) =>(
                 ))
             }
             </div>
-    </div>
-);
+        </div>
+    )
+};
 
 ListaCitas.propTypes = {
     citas : Proptypes.array.isRequired,
